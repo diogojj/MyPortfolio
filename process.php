ff421af7a618fb045960da3bd404f0645006b375
diogo justino <diogo.av.justino@gmail.com>
2020-05-08 07:43:26 +0100

    oi

--- process.php
+++ process.php
@@ -1,5 +1,5 @@
 <?php
-  	require 'assets/sendgrid-php/sendgrid-php.php';
+  	include 'assets/sendgrid-php/sendgrid-php.php';
 	$Name = $_POST["name"];
 	$FromEmail = $_POST["email"];
 	$message = $_POST["message"];
@@ -20,23 +20,18 @@
 	$Body .= "Message: ";
 	$Body .= $message;
 	$Body .= "\n";
+	 
+	
 	// send email
-	echo 'test1';
-	$email->new \SendGrid\Mail\Mail();
-	$email->setFrom($FromEmail, $name);
-	$email->setSubject($Subject);
-	$email->addTo($EmailTo, "Diogo");
-	$email->addContent($Body);
-	echo 'test2';
-	$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-	try {
-    	$response = $sendgrid->send($email);
-    	echo $response->statusCode() . "\n";
-    	echo ($response->headers());
-    	echo $response->body() . "\n";
-	} catch (Exception $e) {
-    	echo 'Caught exception: '. $e->getMessage() ."\n";
-	}
+	$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+	 
+	// redirect to success page
+	if ($success){
+	   echo "success";
+	}else{
+	    echo "invalid";
+	} 
+?>
 ?>
 
 
