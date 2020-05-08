49604d886fc0a7ad03daada6736fe6bee46f3622
diogo justino <diogo.av.justino@gmail.com>
2020-05-08 04:48:59 +0100

    2try

--- process.php
+++ process.php
@@ -30,9 +30,9 @@ require 'assets/sendgrid-php/sendgrid-php.php'
 	$sendgrid = new \SendGrid(getenv('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'));
 	try {
     	$response = $sendgrid->send($email);
-    	print $response->statusCode() . "\n";
-    	print_r($response->headers());
-    	print $response->body() . "\n";
+    	echo $response->statusCode() . "\n";
+    	echo ($response->headers());
+    	echo $response->body() . "\n";
 	} catch (Exception $e) {
     	echo 'Caught exception: '. $e->getMessage() ."\n";
 	}
