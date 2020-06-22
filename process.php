e1741fc367f6bd0621791d9c451c02c160922895
diogo justino <diogo.av.justino@gmail.com>
2020-06-22 21:59:46 +0100

    test02

--- process.php
+++ process.php
@@ -1,16 +1,16 @@
 <?php
   	require 'vendor/autoload.php';
     $Name = $_POST["name"];
+    $Subject = $_POST["subject"];
 	$FromEmail = $_POST["email"];
 	$message = $_POST["message"];
 	// $message = str_replace("\n.", "\n..", $message);
 	$EmailTo = "diogo.av.justino@gmail.com"; 
-	$Subject = $_POST["subject"];
+	
 	 
 	// prepare email body text	 
 	$Body .= "Message: ";
 	$Body .= $message;
-	$Body .= "\n";
 
     $email = new \SendGrid\Mail\Mail();
     $email->setFrom($FromEmail, $Name);
@@ -18,8 +18,10 @@
     $email->addTo($EmailTo);
     $email->addContent("text/plain", $Body);
     $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+    echo getenv('SENDGRID_API_KEY');
     try {
     $response = $sendgrid->send($email);
+    echo $response ;
     } catch (Exception $e) {
         echo 'Caught exception: '. $e->getMessage() ."\n";
     }
